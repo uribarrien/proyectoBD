e57f0f380a25bd4bfe82785555e8cb4e93b3a5f7
Karla <karlagamez155@gmail.com>
2018-11-10 00:18:23 -0600

    Registro proveedores, altas de proveedor

--- php/validar.php
+++ php/validar.php
@@ -10,13 +10,13 @@ $pass = $_POST["password"];
 
 $connection = mysqli_connect($server,$user,$password,$db);
 if(!$connection){
-    echo "Error. Sin conexion a la base de datos".PHP_EOL;
+    echo "Error. Sin conexion a la base de datos";
     echo "Errno de depuracion ".mysqli_connect_errno().PHP_EOL;
     echo "Error de depuracion ".mysqli_connect_error().PHP_EOL;
     exit;
 }
 else{
-    echo "Conexion exitosa<br>".PHP_EOL;
+    echo "Conexion exitosa<br>";
 }
 
 
@@ -26,12 +26,8 @@ $qClient = "SELECT * FROM cliente WHERE correo = '$client' AND contrasena = '$pa
 $resultC = mysqli_query($connection,$qClient);
 $rowsC = mysqli_num_rows($resultC);
 
-session_start();
-$_SESSION['correo'] = $client;
-$_SESSION['password'] = $pass;
-
 if($rowsC > 0){
-    echo "Cliente existe <br>".PHP_EOL;
+    echo "Cliente existe <br>";
 }else{
     echo "REQUIERE INICIAR SESION";
 }
