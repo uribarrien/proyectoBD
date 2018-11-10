66af68b69997b25f7eea5eaa03ce6b464cc15182
Uribarrien Nevares <uribarrien@gmail.com>
2018-11-10 17:30:08 -0600

    registrarCliente.php solo correo y nombre

---
+++ php/registarCliente.php
@@ -0,0 +1,41 @@
+<?php
+
+ $server = "localhost";
+ $db = "proyectobd";
+ $user = "proyecto";
+ $password = "123456789";
+
+ $name = $_POST['nombre'];
+ $apePat = $_POST['apellido_paterno'];
+ $apeMat = $_POST['apellido_materno'];
+ $email = $_POST['correo'];
+ $passwd = $_POST['contrasena'];
+
+ $connection = mysqli_connect($server,$user,$password,$db);
+ if(!$connection){
+
+    echo "Error: No se pudo conectar a la base de datos de MySQL!!".PHP_EOL;
+    echo "errno de depuracion".mysqli_connect_errno().PHP_EOL;
+    echo "error de depuracion".mysqli_connect_error().PHP_EOL;
+    exit;
+
+}
+
+echo "Exito: Se realizo una conexion apropiada a la base de datos. Ahora la base de datos esta lista para usarse".PHP_EOL;
+echo "Informacion del Host:".mysqli_get_host_info($connection).PHP_EOL;
+
+$qInsert = "INSERT INTO cliente(nombre,apellido_paterno,apellido_materno,correo,contrasena) VALUES('$name','$apePat','$apeMat','$email','$passwd');";
+
+mysqli_query($connection,$qInsert);
+
+$qSelect = "SELECT * FROM cliente WHERE correo = '$email' and contrasena = '$passwd';";
+
+$result = mysqli_query($connection,$qSelect);
+$row = mysqli_num_rows($result);
+
+if ($row > 0){
+	echo "Exito en la insersión ";
+} else{
+	echo "Error en la insersión";
+}
+?>
