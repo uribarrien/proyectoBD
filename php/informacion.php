84d2d3d5e10107e8ccd05195f9b66dd327ccb700
Uribarrien Nevares <uribarrien@gmail.com>
2018-11-13 00:41:06 -0600

    en proceso

--- php/informacion.php
+++ php/informacion.php
@@ -9,15 +9,13 @@ session_start();
 $session_email=$_SESSION['correo'];
 error_reporting(0);
 if (isset($_POST["submit"])) {
-    $nombre = $_POST['nombre'];
-$no_exterior = $_POST['num_ext'];
-$colonia = $_POST['colonia'];
-$codigo_postal = $_POST['cp'];
-$estado = $_POST['estado'];
-$ciudad = $_POST['ciudad'];
-    $telefono = $_POST['telefono'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$contrasena = $_POST['contrasena'];
+$confirmacion =$_POST['newcontrasena'];
+$telefono = $_POST['telefono'];
 }
-echo "Hols;"
+
  $nombre = $_POST['nombre'];
 
 echo $nombre;
@@ -35,8 +33,13 @@ if(!$connection){
 echo "Exito: Se realizo una conexion apropiada a la base de datos. Ahora la base de datos esta lista para usarse".PHP_EOL;
 echo "Informacion del Host:".mysqli_get_host_info($connection).PHP_EOL;
 
-$update = "UPDATE cliente SET calle = '$calle', no_exterior = '$no_exterior', colonia = '$colonia', codigo_postal ='$codigo_postal', ciudad_id_ciudad = '$ciudad' WHERE correo = '$session_email'";
+$update = "UPDATE cliente SET nombre = '$nombre', apellido_paterno = '$apellido', telefono ='$telefono' WHERE correo = '$session_email'";
 mysqli_query($connection,$update);
+
+ if ($contrasena == $confirmacion){
+   $update = "UPDATE cliente SET contrasena = '$contrasena' WHERE correo = '$session_email'";
+   mysqli_query($connection,$update);
+ }
 
 $select = "SELECT * FROM cliente WHERE correo = '$session_email';";
 
@@ -45,6 +48,7 @@ $row = mysqli_num_rows($result);
 
 if ($row > 0){
  echo "Exito en la insersión ";
+ header("location:../informacionPersonal.html");
 } else{
  echo "Error en la insersión";
 }
