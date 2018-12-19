755e82d894d0dfb91c14896c1dba4b7d812aac05
Karla <karlagamez155@gmail.com>
2018-12-18 22:57:22 -0600

    Insersion de procedures

--- php/registroProducto.php
+++ php/registroProducto.php
@@ -1,5 +1,4 @@
 <?php
-
 $server = "localhost";
 $db = "proyectobd";
 $user = "proyecto";
@@ -37,37 +36,30 @@ $ins = mysqli_query($connection,$qSelect);
 $qSel = mysqli_fetch_array($ins);
 $id = $qSel['no_producto'];
 echo $id." id producto";
-
-$qInsertPP = "INSERT INTO producto_proveedor(producto_no_producto,proveedor_no_proveedor) VALUES($id,$idP)";
-mysqli_query($connection,$qInsertPP);
+mysqli_query($connection,"CALL insertaPP($id,$idP)");
 
 $colorP = mysqli_query($connection,"SELECT a.nombre color,a.id_color idC, c.no_producto from producto c join color_producto b on(b.producto_no_producto = c.no_producto and c.no_producto=$id) right join color a on( b.color_id_color = a.id_color) order by idC");
 $num = count($color);
 while ($s = mysqli_fetch_array($colorP)) {
   $ban=false;
   for ($i=0; $i < $num; $i++) {
-    if ($s['idC'] == $color[$i] && $s['no_producto'] == null) {
-      $qInsC = "INSERT into color_producto(color_id_color,producto_no_producto) values($color[$i],$id);";
-      mysqli_query($connection,$qInsC);
-      echo "insertar";
-    }
+    $colorI = $color[$i];
+    $idC = $s['idC'];
+    $noP = $s['no_producto'];
+      mysqli_query($connection,"CALL `insertarColor`($idC, $colorI, $noP, $id);");
   }
 }
-
 $tallaP = mysqli_query($connection,"SELECT a.talla talla,a.id_talla idT, c.no_producto from producto c join talla_producto b on(b.producto_no_producto = c.no_producto and c.no_producto=$id) right join talla a on( b.talla_id_talla = a.id_talla) order by idT");
 $num2 = count($talla);
 while ($s = mysqli_fetch_array($tallaP)) {
   for ($i=0; $i < $num2; $i++) {
-    if ($s['idT'] == $talla[$i] && $s['no_producto'] == null) {
-      $qInsT = "INSERT into talla_producto(talla_id_talla,producto_no_producto) values($talla[$i],$id);";
-      mysqli_query($connection,$qInsT);
-      echo "insertar";
-    }
+    $idT = $s['idT'];
+    $tallaI = $talla[$i];
+    $noP = $s['no_producto'];
+      mysqli_query($connection,"CALL insertarTalla($idT,$tallaI, $noP, $id)");
   }
 }
-
 $rows = mysqli_num_rows($ins);
-
 if($rows > 0){
   header("location:../producto.php");
 }else{
